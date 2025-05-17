36fca1b5ae47bdbb0346cf224468c281f4871894
nemtudom2 <szabo.istvan@diak.szbi-pg.hu>
2025-05-17 09:15:21 +0200

    sadf

---
+++ katica/Program.cs
@@ -0,0 +1,12 @@
+﻿using MySql;
+using MySql.Data.MySqlClient;
+Console.WriteLine(asdf.harom());
+MySqlConnection asd = new MySqlConnection("server=database-2.cmz9vjisvcpb.us-east-1.rds.amazonaws.com;database=katica;user=admin;password=almaspite456");
+asd.Open();
+static class asdf
+{
+    public static int harom()
+    {
+        return 3;
+    }
+}
